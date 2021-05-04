3f65ea1c5187df1141859494b26bff0f4606fb58
zhufeng <vzhufeng@163.com>
2021-05-04 10:42:35 +0800

    feat: moify ret

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = new Router();
 
 router.get('/', (ctx) => {
   const { user } = ctx.request.query;
-  ctx.ok({ user });
+  ctx.body = JSON.stringify({ user });
 });
 
 module.exports = router.routes();
