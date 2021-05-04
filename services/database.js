cd5568f93faf296e06101aec3c142489c808f633
zhufeng <vzhufeng@163.com>
2021-05-04 10:59:10 +0800

    feat: services.database dev

--- services/database.js
+++ services/database.js
@@ -1,23 +1,8 @@
+const createModels = require('../models');
+
 module.exports = {
-  //
-  async queryDatabase({ ctx, name }) {
-    return await ctx.conn.execute(
-      `select * from information_schema.SCHEMATA where SCHEMA_NAME = '${name}'`,
-    );
-  },
-  async createDatabase({ ctx, name }) {
-    return await ctx.conn.execute(`create database '${name}'`);
-  },
-  async useDatabase({ ctx, name }) {
-    return await ctx.conn.execute(`use ${name}`);
-  },
-  //
-  async queryTable({ ctx, name }) {
-    return await ctx.conn.execute(
-      `select * from information_schema.TABLES where TABLE_NAME = '${name}'`,
-    );
-  },
-  async createTable({ ctx, name }) {
-    return await ctx.conn.execute(`create table '${name}'`);
+  async initDatabase(ctx) {
+    createModels(ctx);
+    return await ctx.conn.sync();
   },
 };
