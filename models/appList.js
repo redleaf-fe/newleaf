c306ff0698fb66b4379dfafb9a6605c3aaf24ea6
zhufeng <vzhufeng@163.com>
2021-05-04 13:39:56 +0800

    feat: add models.appList

---
+++ models/appList.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (ctx) => {
+  return ctx.conn.define('appList', {
+    name: DataTypes.STRING(20),
+    // 应用唯一Id
+    uid: {
+      type: DataTypes.STRING(20),
+      primaryKey: true,
+    },
+    // 创建人、编辑人
+    createId: DataTypes.STRING(20),
+    updateId: DataTypes.STRING(20),
+    // createdAt
+    // updatedAt
+  });
+};
