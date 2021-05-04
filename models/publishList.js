7ca9fb3455793b3de0313788ef68d22c11841cc7
zhufeng <vzhufeng@163.com>
2021-05-04 13:40:12 +0800

    feat: add models.publishList

---
+++ models/publishList.js
@@ -0,0 +1,21 @@
+const { DataTypes, Sequelize } = require('sequelize');
+
+module.exports = (ctx) => {
+  return ctx.conn.define(
+    'publishList',
+    {
+      branch: DataTypes.STRING(100),
+      commitId: DataTypes.STRING(100),
+      // 发布Id，用时间戳表示
+      uid: {
+        type: DataTypes.DATE,
+        primaryKey: true,
+        defaultValue: Sequelize.NOW
+      },
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    },
+  );
+};
