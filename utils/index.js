90a84fa25b10966080233c5fecef3b89b020a740
vzhufeng <vzhufeng@163.com>
2021-05-13 16:54:11 +0800

    feat(utils): add validate method

--- utils/index.js
+++ utils/index.js
@@ -6,4 +6,13 @@ module.exports = {
       })
       .catch((err) => [err]);
   },
+  validate({ ctx, schema, obj }) {
+    const errors = schema.validate(obj);
+    if (errors.length > 0) {
+      ctx.status = 400;
+      ctx.body = JSON.stringify({ message: errors[0].message });
+      return false;
+    }
+    return true;
+  },
 };
