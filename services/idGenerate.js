6a1335f7bda96b10d7d6429e58c49d0e639fb733
vzhufeng <vzhufeng@163.com>
2021-05-13 17:35:22 +0800

    feat(services): idGenerate dev

--- services/idGenerate.js
+++ services/idGenerate.js
@@ -4,12 +4,14 @@ module.exports = {
   async idGenerate({ conn, modelName }) {
     let uid = nanoid(20);
     let res = await conn.models[modelName].findAll({
+      attributes: ['uid'],
       where: { uid },
     });
 
     while (res.length > 0) {
       uid = nanoid(20);
       res = await conn.models[modelName].findAll({
+        attributes: ['uid'],
         where: { uid },
       });
     }
