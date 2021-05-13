65201adf483c4f5951c084a41c28059a0bef6901
vzhufeng <vzhufeng@163.com>
2021-05-13 17:34:36 +0800

    feat(routes): extract cookie logic

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const crypto = require('crypto');
 const Schema = require('validate');
+
 const { salt } = require('../env.json');
 const { IdGenerate } = require('../services');
 const { validate } = require('../utils');
@@ -34,6 +35,35 @@ const schema = new Schema({
   },
 });
 
+async function setCookie({ ctx, uid, userName }) {
+  // 判断是否已经登录过
+  const res = await ctx.conn.models.login.findAll({
+    attributes: ['uid'],
+    where: { uid },
+  });
+
+  // 生成登录token
+  const token = await IdGenerate.idGenerate({
+    conn: ctx.conn,
+    modelName: 'login',
+  });
+
+  if (res.length > 0) {
+    await ctx.conn.models.login.update({
+      uid,
+      loginToken: token,
+    });
+  } else {
+    await ctx.conn.models.login.create({
+      uid,
+      loginToken: token,
+    });
+  }
+
+  ctx.cookies.set('token', token, cookieConfig);
+  ctx.cookies.set('userName', userName, cookieConfig);
+}
+
 router.post('/login', async (ctx) => {
   const { userName, password } = ctx.request.body;
   if (!validate({ ctx, schema, obj: { userName, password } })) {
@@ -43,25 +73,14 @@ router.post('/login', async (ctx) => {
   const sha256 = crypto.createHash('sha256');
   const encrypt = sha256.update(password + salt).digest('base64');
   const res = await ctx.conn.models.user.findAll({
+    attributes: ['password', 'uid', 'name'],
     where: { name: userName },
   });
 
   if (res.length > 0) {
     // 校验密码
     if (res[0].password === encrypt) {
-      const token = await IdGenerate.idGenerate({
-        conn: ctx.conn,
-        modelName: 'login',
-      });
-
-      // 生成登录token
-      await ctx.conn.models.login.create({
-        uid: res[0].uid,
-        loginToken: token,
-      });
-
-      ctx.cookies.set('token', token, cookieConfig);
-      ctx.cookies.set('userName', res[0].name, cookieConfig);
+      setCookie({ ctx, uid: res[0].uid, userName: res[0].name });
 
       ctx.status = 302;
       ctx.body = JSON.stringify({ redirectUrl: '/dashboard' });
@@ -91,6 +110,7 @@ router.post('/register', async (ctx) => {
 
   // 查询重复的用户名
   const findRepeat = await ctx.conn.models.user.findAll({
+    attributes: ['name'],
     where: { name: userName },
   });
 
@@ -108,19 +128,7 @@ router.post('/register', async (ctx) => {
     uid,
   });
 
-  const token = await IdGenerate.idGenerate({
-    conn: ctx.conn,
-    modelName: 'login',
-  });
-
-  // 生成登录token
-  await ctx.conn.models.login.create({
-    uid,
-    loginToken: token,
-  });
-
-  ctx.cookies.set('token', token, cookieConfig);
-  ctx.cookies.set('userName', userName, cookieConfig);
+  setCookie({ ctx, uid, userName });
 
   ctx.status = 302;
   ctx.body = JSON.stringify({ redirectUrl: '/dashboard' });
