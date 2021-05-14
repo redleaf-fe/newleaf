45abfb4df37327b19c6229e4c9c19b440c774a99
vzhufeng <vzhufeng@163.com>
2021-05-14 11:29:53 +0800

    feat(middlewares): login session validate time

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,3 +1,5 @@
+const { sessionValidTime } = require('../env.json');
+
 module.exports = async (ctx, next) => {
   const whiteList = ['/'];
   if (whiteList.includes(ctx.path)) {
@@ -11,13 +13,18 @@ module.exports = async (ctx, next) => {
     gotoLogin();
   } else {
     const res = await ctx.conn.models.login.findAll({
-      attributes: ['uid'],
-      where: { loginToken: token }
+      attributes: ['uid', 'updatedAt'],
+      where: { loginToken: token },
     });
     if (res.length > 0) {
-      // 方便后面的逻辑获取用户id
-      ctx.uid = res[0].uid;
-      await next();
+      // cookie中的token超过时间也要重新登录
+      if (sessionValidTime <= new Date() - new Date(res[0].updatedAt)) {
+        gotoLogin();
+      } else {
+        // 方便后面的逻辑获取用户id；cookie中有userName，但是因为cookie可以手动修改，所以不使用userName做任何写操作
+        ctx.uid = res[0].uid;
+        await next();
+      }
     } else {
       gotoLogin();
     }
