73cded4b9457dae4b361307350d83586735fbb4c
vzhufeng <vzhufeng@163.com>
2021-05-14 11:22:40 +0800

    feat(routes): del user

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-const router = new Router();
-
-router.get('/', (ctx) => {
-  const { user } = ctx.request.query;
-  ctx.body = JSON.stringify({ user });
-});
-
-module.exports = router.routes();
