ea972c35c09358689e285684a189a0b60abb1e68
vzhufeng <vzhufeng@163.com>
2021-05-14 11:20:04 +0800

    feat(models): modify publishList field

--- models/publishList.js
+++ models/publishList.js
@@ -4,18 +4,18 @@ module.exports = (conn) => {
   return conn.define(
     'publishList',
     {
-      branch: DataTypes.STRING(100),
-      commitId: DataTypes.STRING(100),
+      branch: DataTypes.STRING(200),
+      commitId: DataTypes.STRING(200),
       // 发布Id，用时间戳表示
       uid: {
         type: DataTypes.DATE,
         primaryKey: true,
         defaultValue: Sequelize.NOW
-      },
+      }
     },
     {
-      createdAt: false,
+      // createdAt: false,
       // updatedAt: false,
-    },
+    }
   );
 };
