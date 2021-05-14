ab93dee13c9f8cafb8d192c79ebb61df988ba384
vzhufeng <vzhufeng@163.com>
2021-05-14 11:20:23 +0800

    feat(models): modify use field

--- models/user.js
+++ models/user.js
@@ -5,16 +5,16 @@ module.exports = (conn) => {
   return conn.define(
     'user',
     {
-      name: DataTypes.STRING(20),
+      userName: DataTypes.STRING(20),
       password: DataTypes.STRING(80),
       // 用户Id
       uid: { type: DataTypes.STRING(20), primaryKey: true },
       // 用户所属的应用Id列表
-      appList: DataTypes.STRING,
+      appList: DataTypes.STRING
     },
     {
       createdAt: false,
-      updatedAt: false,
-    },
+      updatedAt: false
+    }
   );
 };
