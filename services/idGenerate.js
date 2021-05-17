d324deeeb057f6c0ec113a8ae6cee6415bccd546
vzhufeng <vzhufeng@163.com>
2021-05-17 15:47:27 +0800

    feat(services): modify idGenerate export and param

--- services/idGenerate.js
+++ services/idGenerate.js
@@ -1,21 +1,19 @@
 const { nanoid } = require('nanoid');
 
-module.exports = {
-  async idGenerate({ conn, modelName }) {
-    let uid = nanoid(20);
-    let res = await conn.models[modelName].findAll({
+module.exports = async ({ ctx, modelName }) => {
+  let uid = nanoid(20);
+  let res = await ctx.conn.models[modelName].findAll({
+    attributes: ['uid'],
+    where: { uid },
+  });
+
+  while (res.length > 0) {
+    uid = nanoid(20);
+    res = await ctx.conn.models[modelName].findAll({
       attributes: ['uid'],
-      where: { uid }
+      where: { uid },
     });
-
-    while (res.length > 0) {
-      uid = nanoid(20);
-      res = await conn.models[modelName].findAll({
-        attributes: ['uid'],
-        where: { uid }
-      });
-    }
-
-    return uid;
   }
+
+  return uid;
 };
