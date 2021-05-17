6ba0e01721bb921af6926981d3e47e70964ba8ec
vzhufeng <vzhufeng@163.com>
2021-05-17 15:37:47 +0800

    feat(models): modify appList field

--- models/appList.js
+++ models/appList.js
@@ -5,17 +5,19 @@ module.exports = (conn) => {
     'appList',
     {
       // 只支持英文、数字和-
-      appName: DataTypes.STRING(20),
+      appName: { type: DataTypes.STRING(20), allowNull: false },
+      // 应用描述
+      desc: DataTypes.STRING(100),
       // 应用唯一Id
-      uid: {
+      id: {
         type: DataTypes.STRING(20),
-        primaryKey: true
+        primaryKey: true,
       },
-      // 创建人、编辑人
-      creator: DataTypes.STRING(20),
-      updater: DataTypes.STRING(20),
-      // git地址
-      git: DataTypes.STRING(200)
+      // 创建人、编辑人 uid
+      creator: { type: DataTypes.STRING(20), allowNull: false },
+      updater: { type: DataTypes.STRING(20), allowNull: false },
+      // git地址，允许为空，部分应用不需要使用发布相关功能
+      git: DataTypes.STRING(200),
     },
     {
       // createdAt: false,
