7b9c31149ba611c78b73fd83f1ebd73e2a1872b7
vzhufeng <vzhufeng@163.com>
2021-05-17 15:40:39 +0800

    feat(routes): app dev

--- routes/app.js
+++ routes/app.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router');
+const { idGenerate, createUniq } = require('../services');
+
 const router = new Router();
 
 router.get('/list', async (ctx) => {
@@ -6,19 +8,66 @@ router.get('/list', async (ctx) => {
   // const { user } = ctx.request.query;
   const res = await ctx.conn.models.user.findAll({
     attributes: ['appList'],
-    where: { uid: ctx.uid }
+    where: { uid: ctx.uid },
   });
   ctx.body = JSON.stringify(res);
 });
 
-router.get('/create', async (ctx) => {
-  const { appName } = ctx.request.body;
+router.get('/save', async (ctx) => {
+  const { appName, git, desc, id } = ctx.request.body;
 
-  await ctx.conn.models.app.create({
-    attributes: ['appList'],
-    where: { uid: ctx.uid }
-  });
-  ctx.body = JSON.stringify({ message: '创建成功' });
+  // 编辑
+  if (id) {
+    const res = await ctx.conn.models.appList.findAll({
+      attributes: ['id'],
+      where: { id },
+    });
+
+    if (res.length > 0) {
+      await ctx.conn.models.appList.update(
+        {
+          appName,
+          desc,
+          updater: ctx.uid,
+          git,
+        },
+        {
+          where: { id },
+        }
+      );
+      
+      ctx.body = JSON.stringify({ message: '保存成功' });
+    } else {
+      ctx.status = 400;
+      ctx.body = JSON.stringify({ message: '未找到应用' });
+    }
+  } else {
+    // 创建
+    const appId = await idGenerate({
+      ctx,
+      modelName: 'appList',
+    });
+
+    if (
+      await createUniq({
+        ctx,
+        modelName: 'appList',
+        queryKey: ['appName'],
+        queryObj: { appName },
+        repeatMsg: '应用名称已被使用',
+        createObj: {
+          appName,
+          id: appId,
+          desc,
+          creator: ctx.uid,
+          updater: ctx.uid,
+          git,
+        },
+      })
+    ) {
+      ctx.body = JSON.stringify({ message: '创建成功' });
+    }
+  }
 });
 
 module.exports = router.routes();
