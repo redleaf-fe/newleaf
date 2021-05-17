f92ec8c441fcc535cd0f6f564e16b7c779bc3704
vzhufeng <vzhufeng@163.com>
2021-05-17 15:45:42 +0800

    feat(services): createUniq modify export

--- services/createUniq.js
+++ services/createUniq.js
@@ -1,27 +1,25 @@
-module.exports = {
-  // 创建时检查重复
-  async createUniq({
-    ctx,
-    modelName,
-    queryKey,
-    queryObj,
-    repeatMsg,
-    createObj
-  }) {
-    // 查询重复的记录
-    const findRepeat = await ctx.conn.models[modelName].findAll({
-      attributes: queryKey,
-      where: queryObj
-    });
+// 创建时检查重复
+module.exports = async ({
+  ctx,
+  modelName,
+  queryKey,
+  queryObj,
+  repeatMsg,
+  createObj,
+}) => {
+  // 查询重复的记录
+  const findRepeat = await ctx.conn.models[modelName].findAll({
+    attributes: queryKey,
+    where: queryObj,
+  });
 
-    if (findRepeat.length > 0) {
-      ctx.status = 400;
-      ctx.body = JSON.stringify({ message: repeatMsg });
-      return false;
-    }
-
-    await ctx.conn.models[modelName].create(createObj);
-
-    return true;
+  if (findRepeat.length > 0) {
+    ctx.status = 400;
+    ctx.body = JSON.stringify({ message: repeatMsg });
+    return false;
   }
+
+  await ctx.conn.models[modelName].create(createObj);
+
+  return true;
 };
