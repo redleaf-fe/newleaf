c8e7cec8de20047540a7b244fae7c76e5a310bfc
vzhufeng <vzhufeng@163.com>
2021-05-31 15:37:40 +0800

    feat(middlewares): change findAll -> findOne

--- middlewares/login.js
+++ middlewares/login.js
@@ -12,20 +12,20 @@ module.exports = async (ctx, next) => {
   if (!token) {
     gotoLogin();
   } else {
-    const res = await ctx.conn.models.login.findAll({
+    const res = await ctx.conn.models.login.findOne({
       attributes: ['uid', 'updatedAt'],
       where: { loginToken: token },
     });
-    if (res.length > 0) {
+    if (res) {
       // cookie中的token超过时间也要重新登录，并删除login中的记录
-      if (sessionValidTime <= new Date() - new Date(res[0].updatedAt)) {
+      if (sessionValidTime <= new Date() - new Date(res.updatedAt)) {
         gotoLogin();
         await ctx.conn.models.login.destroy({
           where: { loginToken: token },
         });
       } else {
         // 方便后面的逻辑获取用户id；cookie中有userName，但是因为cookie可以手动修改，所以不使用userName做任何写操作
-        ctx.uid = res[0].uid;
+        ctx.uid = res.uid;
         await next();
       }
     } else {
