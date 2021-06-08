480a1012d9eed1c1de61b9b8e944a836ea67c58d
vzhufeng <vzhufeng@163.com>
2021-06-08 17:05:32 +0800

    feat(routes): 修改user存储applist的格式,增加detail接口

--- routes/app.js
+++ routes/app.js
@@ -21,19 +21,6 @@ const schema = new Schema({
   },
 });
 
-router.get('/ids', async (ctx) => {
-  const res = await ctx.conn.models.user.findOne({
-    attributes: ['appList'],
-    where: { uid: ctx.uid },
-  });
-
-  if (res) {
-    ctx.body = JSON.stringify(res.appList.split(','));
-  } else {
-    ctx.body = JSON.stringify({ message: '未找到用户' });
-  }
-});
-
 router.get('/list', async (ctx) => {
   // todo: 搜索条件
   // const { user } = ctx.request.query;
@@ -45,13 +32,24 @@ router.get('/list', async (ctx) => {
   if (res) {
     // 查找appList表获取详情
     const res2 = await ctx.conn.models.appList.findAll({
-      attributes: ['appName', 'git', 'updatedAt'],
-      where: { id: res.appList.split(',') },
-    })
+      attributes: ['appName', 'git', 'updatedAt', 'id'],
+      where: { id: Object.values(JSON.parse(res.appList)).map((v) => v.id) },
+    });
     ctx.body = res2;
   } else {
-    ctx.body = JSON.stringify({ message: '未找到用户' });
+    ctx.body = { message: '未找到用户' };
   }
+});
+
+router.get('/detail', async (ctx) => {
+  const { id = '' } = ctx.request.query;
+
+  const res = await ctx.conn.models.appList.findOne({
+    attributes: ['appName', 'desc', 'git'],
+    where: { id },
+  });
+
+  ctx.body = res || { message: '未找到应用' };
 });
 
 router.post('/save', async (ctx) => {
@@ -81,10 +79,10 @@ router.post('/save', async (ctx) => {
         }
       );
 
-      ctx.body = JSON.stringify({ message: '保存成功' });
+      ctx.body = { message: '保存成功' };
     } else {
       ctx.status = 400;
-      ctx.body = JSON.stringify({ message: '未找到应用' });
+      ctx.body = { message: '未找到应用' };
     }
   } else {
     // 创建
@@ -119,13 +117,17 @@ router.post('/save', async (ctx) => {
       await ctx.conn.models.user.update(
         {
           // appList为空，不进行拼接
-          appList: res.appList ? `${res.appList},${appId}` : appId,
+          appList: res.appList
+            ? JSON.stringify(
+                JSON.parse(res.appList).concat({ id: appId, auth: 'admin' })
+              )
+            : JSON.stringify([{ id: appId, auth: 'admin' }]),
         },
         {
           where: { uid: ctx.uid },
         }
       );
-      ctx.body = JSON.stringify({ message: '创建成功' });
+      ctx.body = { message: '创建成功' };
     }
   }
 });
