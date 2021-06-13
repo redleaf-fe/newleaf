02d8851d79ec489071a4cfdcbe155002e6cc078b
vzhufeng <vzhufeng@163.com>
2021-06-13 23:40:44 +0800

    feat: update routes

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ module.exports = (router) => {
   router.use('/login', require('./login'));
   router.use('/app', require('./app'));
   router.use('/log', require('./log'));
+  router.use('/user', require('./user'));
+  router.use('/userApp', require('./userApp'));
 };
