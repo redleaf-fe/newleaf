247cee788933bf158781944fc73b9362a9813e0d
vzhufeng <vzhufeng@163.com>
2021-06-22 15:31:03 +0800

    feat(models): del useless model temporary

--- models/index.js
+++ models/index.js
@@ -2,8 +2,6 @@ module.exports = (conn) => {
   return {
     user: require('./user')(conn),
     login: require('./login')(conn),
-    app: require('./app')(conn),
     appPublish: require('./appPublish')(conn),
-    userApp: require('./userApp')(conn),
   };
 };
