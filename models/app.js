806fc4c1828c1c721b1cd96a00621e3a099043a1
vzhufeng <vzhufeng@163.com>
2021-06-23 09:42:21 +0800

    feat(model): add app model

---
+++ models/app.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (conn) => {
+  return conn.define(
+    'app',
+    {
+      // 只支持英文、数字和-
+      appName: { type: DataTypes.STRING(20), primaryKey: true },
+      // git内Id
+      gitId: { type: DataTypes.STRING(20), allowNull: false },
+      // 创建人、编辑人
+      creator: { type: DataTypes.STRING(20), allowNull: false },
+      updater: { type: DataTypes.STRING(20), allowNull: false },
+      // git地址，允许为空，部分应用不需要使用发布相关功能
+      git: DataTypes.STRING(300),
+    },
+    {
+      // createdAt: false,
+      // updatedAt: false,
+    }
+  );
+};
