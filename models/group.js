cb4545433e6b3468c9e48bc8dab6455de4e5a419
vzhufeng <vzhufeng@163.com>
2021-06-23 09:42:04 +0800

    feat(model): add group

---
+++ models/group.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (conn) => {
+  return conn.define(
+    'group',
+    {
+      // 只支持英文、数字和-
+      groupName: { type: DataTypes.STRING(20), primaryKey: true },
+      // git内Id
+      gitId: { type: DataTypes.STRING(20), allowNull: false },
+      // 创建人、编辑人
+      creator: { type: DataTypes.STRING(20), allowNull: false },
+      updater: { type: DataTypes.STRING(20), allowNull: false },
+    },
+    {
+      // createdAt: false,
+      // updatedAt: false,
+    }
+  );
+};
