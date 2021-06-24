4902a92f9de17d32e864bb0112baa014b3ae022a
vzhufeng <vzhufeng@163.com>
2021-06-24 16:42:47 +0800

    feat(route): userApp dev

--- routes/userApp.js
+++ routes/userApp.js
@@ -1,19 +1,40 @@
 const Router = require('koa-router');
+const { searchAndPage } = require('../utils');
+const { maxPageSize } = require('../const');
 
 const router = new Router();
 
 router.get('/list', async (ctx) => {
-  const { id, currentPage = 1, pageSize = 10 } = ctx.request.query;
-
+  const { id, name, currentPage = 1, pageSize = 10 } = ctx.request.query;
   if (id) {
-    const res = await ctx.codeRepo.getGroupMembers({
-      id,
-      page: currentPage,
-      per_page: pageSize,
-    });
+    let res;
+    if (name) {
+      res = await ctx.codeRepo.getProjectMembers({
+        id,
+        page: 1,
+        per_page: maxPageSize,
+      });
+      // 过滤掉root
+      res.data = res.data.filter((v) => v.username !== 'root');
+      res = searchAndPage({
+        data: res.data,
+        currentPage,
+        pageSize,
+        search: name,
+        searchKey: 'username',
+      });
+    } else {
+      res = await ctx.codeRepo.getProjectMembers({
+        id,
+        page: currentPage,
+        per_page: pageSize,
+      });
+      // 过滤掉root
+      res.data = res.data.filter((v) => v.username !== 'root');
+    }
 
     ctx.body = {
-      count: res.total,
+      count: res.total - 1,
       rows: res.data,
     };
   } else {
@@ -21,11 +42,31 @@ router.get('/list', async (ctx) => {
   }
 });
 
-router.post('/delete', async (ctx) => {
-  const { gitUid, groupId } = ctx.request.body;
+async function hasAccess({ ctx, id, user_id }) {
+  const res = await ctx.codeRepo.getUserOfProject({ id, user_id });
 
-  await ctx.codeRepo.removeUserFromGroup({
-    group_id: groupId,
+  if (res.access_level < 40) {
+    return false;
+  }
+
+  // 不能操作自己
+  if (+ctx.gitUid === +user_id) {
+    return false;
+  }
+
+  return true;
+}
+
+router.post('/delete', async (ctx) => {
+  const { gitUid, id } = ctx.request.body;
+
+  if (!(await hasAccess({ ctx, id, user_id: gitUid }))) {
+    ctx.body = { message: '没有操作权限' };
+    return;
+  }
+
+  await ctx.codeRepo.removeUserFromProject({
+    id,
     user_id: gitUid,
   });
 
@@ -33,7 +74,7 @@ router.post('/delete', async (ctx) => {
 });
 
 router.post('/save', async (ctx) => {
-  const { uid, gitUid, groupId, access } = ctx.request.body;
+  const { uid, gitUid, id, access } = ctx.request.body;
 
   if (uid) {
     // 创建
@@ -42,16 +83,26 @@ router.post('/save', async (ctx) => {
       where: { uid },
     });
 
-    await ctx.codeRepo.addUserIntoGroup({
-      group_id: groupId,
+    if (!(await hasAccess({ ctx, id, user_id: res.gitUid }))) {
+      ctx.body = { message: '没有操作权限' };
+      return;
+    }
+
+    await ctx.codeRepo.addUserIntoProject({
+      id,
       user_id: res.gitUid,
       access_level: access || 30,
     });
     ctx.body = { message: '操作成功' };
   } else if (gitUid) {
+    if (!(await hasAccess({ ctx, id, user_id: gitUid }))) {
+      ctx.body = { message: '没有操作权限' };
+      return;
+    }
+
     // 编辑
-    await ctx.codeRepo.editUserInGroup({
-      group_id: groupId,
+    await ctx.codeRepo.editUserInProject({
+      id,
       user_id: gitUid,
       access_level: access || 30,
     });
