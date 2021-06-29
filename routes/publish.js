a47e34102d0c1634adafe55da4e67eab46f4a206
vzhufeng <vzhufeng@163.com>
2021-06-29 21:12:23 +0800

    feat(route): publish dev

--- routes/publish.js
+++ routes/publish.js
@@ -2,13 +2,12 @@ const Router = require('koa-router');
 const Schema = require('validate');
 const { Op } = require('sequelize');
 
-const { idGenerate, createUniq } = require('../services');
 const { validate } = require('../utils');
 
 const router = new Router();
 
 const schema = new Schema({
-  appName: {
+  name: {
     type: String,
     required: true,
     length: { max: 20 },
@@ -17,15 +16,46 @@ const schema = new Schema({
       length: '发布名称不大于20字符',
     },
   },
+  appId: {
+    type: String,
+    required: true,
+    length: { max: 20 },
+    message: {
+      required: '应用Id必填',
+      length: '应用Id不大于20字符',
+    },
+  },
+  appName: {
+    type: String,
+    required: true,
+    length: { max: 20 },
+    message: {
+      required: '应用名称必填',
+      length: '应用名称不大于20字符',
+    },
+  },
+  branch: {
+    type: String,
+    required: true,
+    length: { max: 200 },
+    message: {
+      required: '分支名必填',
+      length: '分支名不大于200字符',
+    },
+  },
+  commitId: {
+    type: String,
+    required: true,
+    length: { max: 200 },
+    message: {
+      required: '提交Id必填',
+      length: '提交Id不大于200字符',
+    },
+  },
 });
 
 router.get('/list', async (ctx) => {
-  const {
-    appId,
-    publishName,
-    currentPage = 1,
-    pageSize = 10,
-  } = ctx.request.query;
+  const { appId, name, currentPage = 1, pageSize = 10 } = ctx.request.query;
 
   if (!appId) {
     ctx.body = { message: '应用id必填' };
@@ -40,8 +70,8 @@ router.get('/list', async (ctx) => {
     ],
   };
 
-  if (publishName) {
-    filter[Op.and].push({ name: { [Op.like]: `%${publishName}%` } });
+  if (name) {
+    filter[Op.and].push({ name: { [Op.like]: `%${name}%` } });
   }
 
   const publish = await ctx.conn.models.appPublish.findAndCountAll({
@@ -54,89 +84,36 @@ router.get('/list', async (ctx) => {
   ctx.body = publish;
 });
 
-router.get('/detail', async (ctx) => {
-  const { id = '' } = ctx.request.query;
-
-  const res = await ctx.conn.models.app.findOne({
-    attributes: ['name', 'desc', 'git'],
-    where: { id },
-  });
-
-  ctx.body = res || { message: '未找到应用' };
+router.get('/approve', async (ctx) => {
+  // const { id = '' } = ctx.request.query;
+  // const res = await ctx.conn.models.app.findOne({
+  //   attributes: ['name', 'desc', 'git'],
+  //   where: { id },
+  // });
+  // ctx.body = res || { message: '未找到应用' };
 });
 
 router.post('/save', async (ctx) => {
-  const { appName, git, desc, id } = ctx.request.body;
+  const { name, appId, appName, desc, branch, commitId } = ctx.request.body;
 
-  if (!validate({ ctx, schema, obj: { appName } })) {
+  if (
+    !validate({ ctx, schema, obj: { name, appId, appName, branch, commitId } })
+  ) {
     return;
   }
 
-  // 编辑
-  if (id) {
-    const res = await ctx.conn.models.app.findOne({
-      attributes: ['id'],
-      where: { id },
-    });
+  // 允许发布同名
 
-    if (res) {
-      await ctx.conn.models.app.update(
-        {
-          name: appName,
-          desc,
-          updater: ctx.uid,
-          git,
-        },
-        {
-          where: { id },
-        }
-      );
-
-      ctx.body = { message: '保存成功' };
-    } else {
-      ctx.status = 400;
-      ctx.body = { message: '未找到应用' };
-    }
-  } else {
-    // 创建
-    const appId = await idGenerate({
-      ctx,
-      modelName: 'app',
-      idName: 'id',
-    });
-
-    if (
-      await createUniq({
-        ctx,
-        modelName: 'app',
-        queryKey: ['appName'],
-        queryObj: { appName },
-        repeatMsg: '应用名称已被使用',
-        createObj: {
-          appName,
-          id: appId,
-          desc,
-          creator: ctx.uid,
-          updater: ctx.uid,
-          git,
-        },
-      })
-    ) {
-      const res = await ctx.conn.models.user.findOne({
-        attributes: ['username'],
-        where: { uid: ctx.uid },
-      });
-
-      await ctx.conn.models.userApp.create({
-        uid: ctx.uid,
-        username: res.username,
-        appId,
-        appName,
-        auth: 'admin',
-      });
-      ctx.body = { message: '创建成功' };
-    }
-  }
+  await ctx.conn.models.publish.create({
+    name,
+    appId,
+    appName,
+    desc,
+    branch,
+    commitId,
+    creator: ctx.uid,
+  });
+  ctx.body = { message: '创建成功' };
 });
 
 module.exports = router.routes();
