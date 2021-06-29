bc3d95cea8f21b53b1a6434877c55a02109a17e5
vzhufeng <vzhufeng@163.com>
2021-06-29 15:58:15 +0800

    feat(service): move method from file to index

--- services/index.js
+++ services/index.js
@@ -1,7 +1,88 @@
+const { customAlphabet } = require('nanoid');
+
+const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 20);
+
 module.exports = {
   Database: require('./database'),
   CodeRepo: require('./codeRepo'),
-  idGenerate: require('./idGenerate'),
-  createUniq: require('./createUniq'),
-  findRepeat: require('./findRepeat'),
+
+  async idGenerate ({ ctx, modelName, idName }) {
+    let id = nanoid();
+    let res = await ctx.conn.models[modelName].findOne({
+      attributes: [idName],
+      where: { [idName]: id },
+    });
+  
+    while (res) {
+      id = nanoid();
+      res = await ctx.conn.models[modelName].findOne({
+        attributes: [idName],
+        where: { [idName]: id },
+      });
+    }
+  
+    return id;
+  },
+
+  async createUniq({
+    ctx,
+    modelName,
+    queryKey,
+    queryObj,
+    repeatMsg,
+    createObj,
+  }) {
+    // 查询重复的记录
+    const findRepeat = await ctx.conn.models[modelName].findOne({
+      attributes: queryKey,
+      where: queryObj,
+    });
+
+    if (findRepeat) {
+      ctx.status = 400;
+      ctx.body = { message: repeatMsg };
+      return false;
+    }
+
+    await ctx.conn.models[modelName].create(createObj);
+
+    return true;
+  },
+
+  async findRepeat({ ctx, modelName, queryKey, queryObj, repeatMsg }) {
+    // 查询重复的记录
+    const findRepeat = await ctx.conn.models[modelName].findOne({
+      attributes: queryKey,
+      where: queryObj,
+    });
+
+    if (findRepeat) {
+      ctx.status = 400;
+      ctx.body = { message: repeatMsg };
+      return true;
+    }
+
+    return false;
+  },
+
+  async namespaceHasAccess({ ctx, id, user_id, type }) {
+    // 项目和分组操作是否有权限
+    const reqMap = {
+      group: ctx.codeRepo.getUserOfGroup,
+      app: ctx.codeRepo.getUserOfProject,
+    };
+
+    const res = await reqMap[type]({ id, user_id: ctx.gitUid });
+
+    if (res.data.access_level < 40) {
+      return false;
+    }
+
+    // 不能操作自己
+    if (+ctx.gitUid === +user_id) {
+      return false;
+    }
+
+    return true;
+  },
 };
