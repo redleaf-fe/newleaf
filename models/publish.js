97c9ecdfdf4afda5f32056729f21784317698332
vzhufeng <vzhufeng@163.com>
2021-07-01 21:27:35 +0800

    feat(model): update publish

--- models/publish.js
+++ models/publish.js
@@ -5,18 +5,21 @@ module.exports = (conn) => {
   return conn.define(
     'publish',
     {
-      branch: { type: DataTypes.STRING(200), allowNull: false },
-      commitId: { type: DataTypes.STRING(200), allowNull: false },
+      branch: { type: DataTypes.STRING(100), allowNull: false },
+      commitId: { type: DataTypes.STRING(50), allowNull: false },
       desc: DataTypes.STRING(100),
       name: { type: DataTypes.STRING(20), allowNull: false },
       appId: { type: DataTypes.STRING(20), allowNull: false },
       appName: { type: DataTypes.STRING(20), allowNull: false },
+      // 发布阶段
+      stage: { type: DataTypes.ENUM('null', 'daily', 'pre', 'prod'), allowNull: false },
+      // 审批实例Id
+      aId: { type: DataTypes.STRING(20), allowNull: false },
       // 创建人
       creator: { type: DataTypes.STRING(20), allowNull: false },
+      creatorId: { type: DataTypes.STRING(20), allowNull: false },
     },
     {
-      // createdAt: false,
-      // updatedAt: false,
       charset: 'utf8',
     }
   );
