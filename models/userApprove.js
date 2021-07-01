3e347b1a77618ed4ef5b8351c0b16c58aa93ae86
vzhufeng <vzhufeng@163.com>
2021-07-01 21:25:18 +0800

    feat(model): add user approve

---
+++ models/userApprove.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+
+// 记录用户需要审批的实例
+module.exports = (conn) => {
+  return conn.define(
+    'user',
+    {
+      // 用户Id
+      uid: { type: DataTypes.STRING(20), primaryKey: true },
+      // 审批实例Id
+      aId: { type: DataTypes.STRING(20), allowNull: false },
+      // 审批类型
+      type: { type: DataTypes.ENUM('publish'), allowNull: false },
+    },
+    {
+      charset: 'utf8',
+    }
+  );
+};
