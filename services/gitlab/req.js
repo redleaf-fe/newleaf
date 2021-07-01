5e339c5605242d863551650bf51203c1a864e9d0
vzhufeng <vzhufeng@163.com>
2021-07-01 14:25:05 +0800

    feat(service): update gitServer config

--- services/gitlab/req.js
+++ services/gitlab/req.js
@@ -3,7 +3,7 @@ const config = require('../../env.json');
 
 module.exports = async function ({ url, method = 'get', data = {} }) {
   const param = {
-    baseURL: config.gitSeverPath,
+    baseURL: config.gitSever + '/api/v4/',
     method,
     headers: { 'PRIVATE-TOKEN': config.gitToken },
     url,
