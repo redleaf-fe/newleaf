9a7fa7d116fc7dc3ac966aea75bb54bb50cfa57f
vzhufeng <vzhufeng@163.com>
2021-07-08 21:06:01 +0800

    feat(route): publish dev

--- routes/publish.js
+++ routes/publish.js
@@ -3,19 +3,11 @@ const Schema = require('validate');
 const { Op } = require('sequelize');
 
 const { validate } = require('../utils');
+const { envMap } = require('../const');
 
 const router = new Router();
 
 const schema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    length: { max: 100 },
-    message: {
-      required: '发布名称必填',
-      length: '发布名称不大于100字符',
-    },
-  },
   appId: {
     required: true,
     length: { max: 20 },
@@ -33,7 +25,7 @@ const schema = new Schema({
       length: '应用名称不大于100字符',
     },
   },
-  branch: {
+  commit: {
     type: String,
     required: true,
     length: { max: 100 },
@@ -42,21 +34,13 @@ const schema = new Schema({
       length: '分支名不大于100字符',
     },
   },
-  commitId: {
-    type: String,
-    required: true,
-    length: { max: 100 },
-    message: {
-      required: '提交Id必填',
-      length: '提交Id不大于100字符',
-    },
-  },
 });
 
 router.get('/list', async (ctx) => {
-  const { appId, name, currentPage = 1, pageSize = 10 } = ctx.request.query;
+  const { appId, env, currentPage = 1, pageSize = 10 } = ctx.request.query;
 
   if (!appId) {
+    ctx.status = 400;
     ctx.body = { message: '应用id必填' };
     return;
   }
@@ -65,13 +49,10 @@ router.get('/list', async (ctx) => {
     [Op.and]: [
       {
         appId,
+        env
       },
     ],
   };
-
-  if (name) {
-    filter[Op.and].push({ name: { [Op.like]: `%${name}%` } });
-  }
 
   const publish = await ctx.conn.models.publish.findAndCountAll({
     offset: pageSize * (currentPage - 1),
@@ -89,49 +70,60 @@ router.get('/approve', async (ctx) => {
   //   attributes: ['name', 'desc', 'git'],
   //   where: { id },
   // });
+  // ctx.status = 400;
   // ctx.body = res || { message: '未找到应用' };
 });
 
 router.post('/save', async (ctx) => {
-  const { name, appId, appName, desc, branch, commitId } = ctx.request.body;
-  // 允许发布同名
-  if (
-    !validate({ ctx, schema, obj: { name, appId, appName, branch, commitId } })
-  ) {
+  const { appId, appName, desc, commit, env } = ctx.request.body;
+  if (!validate({ ctx, schema, obj: { appId, appName, commit } })) {
     return;
   }
 
-  const res = await ctx.conn.models.app.findOne({
-    where: { gitId: appId },
-  });
+  let approveInsId;
 
-  if (!res.apId) {
-    ctx.body = { message: '获取应用审核环节失败' };
-    return;
-  }
-  // 创建审批实例
-  const res2 = await ctx.conn.models.approveIns.create({
-    stageId: 0,
-    status: 'pending',
-    apId: res.apId,
-  });
+  if (env === envMap.prod) {
+    const res = await ctx.conn.models.app.findOne({
+      where: { gitId: appId },
+    });
 
-  if (!res2.id) {
-    ctx.body = { message: '创建审核实例失败' };
-    return;
+    if (!res.apId) {
+      ctx.status = 400;
+      ctx.body = { message: '获取应用审核环节失败' };
+      return;
+    }
+
+    // 创建审批实例
+    const res2 = await ctx.conn.models.approveIns.create({
+      stageId: 0,
+      status: 'pending',
+      apId: res.apId,
+    });
+
+    if (!res2.id) {
+      ctx.status = 400;
+      ctx.body = { message: '创建审核实例失败' };
+      return;
+    }
+
+    approveInsId = res2.id;
   }
-  await ctx.conn.models.publish.create({
-    name,
+
+  const param = {
     appId,
     appName,
     desc,
-    branch,
-    commitId,
+    commit,
     creatorId: ctx.uid,
     creator: ctx.username,
-    env: 'daily',
-    aId: res2.id,
-  });
+    env,
+  };
+
+  if (approveInsId) {
+    param.aId = approveInsId;
+  }
+
+  await ctx.conn.models.publish.create(param);
   ctx.body = { message: '创建成功' };
 });
 
