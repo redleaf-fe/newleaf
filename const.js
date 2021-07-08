143a2bdf729164c2d765b5708093bea49d89381b
vzhufeng <vzhufeng@163.com>
2021-07-08 21:06:53 +0800

    feat: add const

--- const.js
+++ const.js
@@ -1,1 +1,20 @@
-module.exports = {};
+module.exports = {
+  // 环境
+  envMap: {
+    daily: 'daily',
+    pre: 'pre',
+    perf: 'perf',
+    prod: 'prod',
+  },
+  // 审批状态
+  approveStatusMap: {
+    pending: 'pending',
+    doing: 'doing',
+    done: 'done',
+    fail: 'fail',
+  },
+  // 审批类型
+  approveTypeMap: {
+    publish: 'publish',
+  },
+};
