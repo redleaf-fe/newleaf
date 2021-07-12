bef5ee23fcaf0ca7447dc4429ce6e180ad46c0d6
vzhufeng <vzhufeng@163.com>
2021-07-12 11:02:48 +0800

    feat(route): publish dev

--- routes/publish.js
+++ routes/publish.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 const Schema = require('validate');
 const { Op } = require('sequelize');
+const axios = require('axios');
+const config = require('../env.json');
 
 const { validate } = require('../utils');
-const { envMap } = require('../const');
+const { envMap, publishStatusMap, approveStatusMap } = require('../const');
 
 const router = new Router();
 
@@ -25,13 +27,22 @@ const schema = new Schema({
       length: '应用名称不大于100字符',
     },
   },
-  commit: {
+  branch: {
     type: String,
     required: true,
     length: { max: 100 },
     message: {
       required: '分支名必填',
       length: '分支名不大于100字符',
+    },
+  },
+  commit: {
+    type: String,
+    required: true,
+    length: { max: 100 },
+    message: {
+      required: '提交Id必填',
+      length: '提交Id不大于100字符',
     },
   },
 });
@@ -49,7 +60,7 @@ router.get('/list', async (ctx) => {
     [Op.and]: [
       {
         appId,
-        env
+        env,
       },
     ],
   };
@@ -64,19 +75,9 @@ router.get('/list', async (ctx) => {
   ctx.body = publish;
 });
 
-router.get('/approve', async (ctx) => {
-  // const { id = '' } = ctx.request.query;
-  // const res = await ctx.conn.models.app.findOne({
-  //   attributes: ['name', 'desc', 'git'],
-  //   where: { id },
-  // });
-  // ctx.status = 400;
-  // ctx.body = res || { message: '未找到应用' };
-});
-
 router.post('/save', async (ctx) => {
-  const { appId, appName, desc, commit, env } = ctx.request.body;
-  if (!validate({ ctx, schema, obj: { appId, appName, commit } })) {
+  const { appId, appName, desc = '', branch, commit, env } = ctx.request.body;
+  if (!validate({ ctx, schema, obj: { appId, appName, branch, commit } })) {
     return;
   }
 
@@ -113,10 +114,12 @@ router.post('/save', async (ctx) => {
     appId,
     appName,
     desc,
+    branch,
     commit,
     creatorId: ctx.uid,
     creator: ctx.username,
     env,
+    status: publishStatusMap.pending,
   };
 
   if (approveInsId) {
@@ -125,6 +128,87 @@ router.post('/save', async (ctx) => {
 
   await ctx.conn.models.publish.create(param);
   ctx.body = { message: '创建成功' };
+});
+
+router.post('/result', async (ctx) => {
+
+});
+
+router.post('/build', async (ctx) => {
+  const { id = '', env } = ctx.request.body;
+
+  const res = await ctx.conn.models.publish.findOne({
+    where: {
+      id,
+    },
+  });
+
+  // 生产环境发布需要审核通过
+  if (env === envMap.prod) {
+    if (res.aId) {
+      const res2 = await ctx.conn.models.approveIns.findOne({
+        where: {
+          id: res.aId,
+        },
+      });
+      if (res2.status !== approveStatusMap.done) {
+        ctx.status = 400;
+        ctx.body = { message: '审核流程未完成' };
+        return;
+      }
+    } else {
+      ctx.status = 400;
+      ctx.body = { message: '生产环境发布必须有审核流程' };
+      return;
+    }
+  }
+
+  // 判断是否有别人在发布
+  const appInfo = await ctx.conn.models.app.findOne({ gitId: res.appId });
+  const isPublishing = JSON.parse(appInfo.isPublishing);
+  if (isPublishing.includes(env)) {
+    ctx.status = 400;
+    ctx.body = { message: '项目发布中' };
+    return;
+  }
+
+  const appDetail = await ctx.codeRepo.getProjectDetail({ id: res.appId });
+  if (!appDetail.data) {
+    ctx.status = 400;
+    ctx.body = { message: '获取项目详情失败' };
+    return;
+  }
+
+  const { branch, commit, appName } = res;
+  const res2 = await axios({
+    url: `${config.publishSever}/build/build`,
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    data: {
+      appName,
+      gitPath: `http://user1:11111111@${config.gitSever}/${appDetail.data.path_with_namespace}`,
+      branch,
+      commit,
+      id
+    },
+  });
+
+  if (res2.message === 'ok') {
+    isPublishing.push(env);
+    await ctx.conn.models.app.update(
+      {
+        isPublishing: JSON.stringify(isPublishing),
+      },
+      {
+        where: { gitId: res.appId },
+      }
+    );
+    ctx.body = { message: 'ok' };
+    return;
+  }
+
+  ctx.status = 400;
+  ctx.body = { message: res2.response.data.message };
 });
 
 module.exports = router.routes();
