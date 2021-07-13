4d462d178c3ea2e6357c14ffbc4b023a1bdf9571
vzhufeng <vzhufeng@163.com>
2021-07-13 10:48:49 +0800

    feat: update const

--- const.js
+++ const.js
@@ -1,3 +1,10 @@
+const flowMap = {
+  pending: 'pending',
+  doing: 'doing',
+  done: 'done',
+  fail: 'fail',
+};
+
 module.exports = {
   // 环境
   envMap: {
@@ -6,13 +13,12 @@ module.exports = {
     perf: 'perf',
     prod: 'prod',
   },
+  // 打包状态
+  buildStatusMap: flowMap,
+  // 发布状态
+  publishStatusMap: flowMap,
   // 审批状态
-  approveStatusMap: {
-    pending: 'pending',
-    doing: 'doing',
-    done: 'done',
-    fail: 'fail',
-  },
+  approveStatusMap: flowMap,
   // 审批类型
   approveTypeMap: {
     publish: 'publish',
