39d002eedcda099d40e480e3b5e51b9baddb7d5f
vzhufeng <vzhufeng@163.com>
2021-07-19 09:38:12 +0800

    feat(model): update approveProto

--- models/approveProto.js
+++ models/approveProto.js
@@ -5,6 +5,8 @@ module.exports = (conn) => {
   return conn.define(
     'approveProto',
     {
+      // id会默认生成
+      
       // 环节详情，上限10个环节，每个环节最多5个人
       /*
         n: xxx 审批人姓名
@@ -12,7 +14,7 @@ module.exports = (conn) => {
         [[{ n:xxx, i:yyy }, ...], [...]]
        */
       stage: {
-        type: DataTypes.STRING(10000),
+        type: DataTypes.TEXT,
         allowNull: false,
       },
     },
